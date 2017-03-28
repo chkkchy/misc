1e19368e6d13d76dd3d566863cd7d7349208e8d0
Tamura Yukichi <tamura.yukichi@gmail.com>
2017-03-28 23:54:43 +0900

    [from now] 2017/03/28 23:54:43
     main.go | 13 ++++++++++++-
     1 file changed, 12 insertions(+), 1 deletion(-)

--- main.go
+++ main.go
@@ -7,11 +7,22 @@ import (
 	"github.com/labstack/echo"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
 	fmt.Println("vim-go")
+
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hoge")
+		u := new(User)
+		u.Name = "yukichi"
+		u.Age = 13
+		return c.JSON(http.StatusOK, u)
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
