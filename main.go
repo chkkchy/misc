7f19e05f36958ce4bde5a16ee8fbd22ebf4d913a
Tamura Yukichi <tamura.yukichi@gmail.com>
2017-03-29 00:23:31 +0900

    [from now] 2017/03/29 00:23:31
     main.go | 76 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     1 file changed, 76 insertions(+)

--- main.go
+++ main.go
@@ -1,12 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 )
 
+const (
+	hokkaido int = iota + 1
+	aomori
+	iwate
+	miyagi
+	akita
+	yamagata
+	fukushima
+	ibaraki
+	tochigi
+	gunma
+	saitama
+	chiba
+	toukyo
+	kanagawa
+	niigata
+	toyama
+	ishikawa
+	fukui
+	yamanashi
+	nagano
+	gifu
+	sizuoka
+	aichi
+	mie
+	shiga
+	kyoto
+	osaka
+	hyogo
+	nara
+	wakayama
+	tottori
+	shimane
+	okayama
+	hiroshima
+	yamaguchi
+	tokushima
+	kagawa
+	ehime
+	kochi
+	fukuoka
+	saga
+	nagasaki
+	kumamoto
+	oita
+	miyazaki
+	kagoshima
+	okinawa
+)
+
+// User is user
 type User struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
@@ -22,6 +75,29 @@ func main() {
 		u.Name = "yukichi"
 		u.Age = 13
 		return c.JSON(http.StatusOK, u)
+	})
+
+	e.GET("/weather", func(c echo.Context) error {
+		fmt.Printf("%01d\n", 2)
+		fmt.Println(okinawa)
+
+		url := "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040"
+		req, _ := http.NewRequest("GET", url, nil)
+		client := &http.Client{
+			Timeout: 5 * time.Second,
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		type res struct {
+			Title string `json:"title"`
+			Link  string `json:"link"`
+		}
+		r := new(res)
+		err = json.NewDecoder(resp.Body).Decode(r)
+		return c.JSON(http.StatusOK, r)
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
