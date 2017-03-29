050fb8c38ea0c0876be7e07895903e4b7c51f1f8
Tamura Yukichi <tamura.yukichi@gmail.com>
2017-03-29 14:46:05 +0900

    [from now] 2017/03/29 14:46:05
     api/weather.go   |  89 ++++++++++++++++++++++++++++++++++++++++++++++++++++++
     main.go          |  90 ++-----------------------------------------------------
     tmp/runner-build | Bin 0 -> 6895716 bytes
     3 files changed, 91 insertions(+), 88 deletions(-)

---
+++ api/weather.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+)
+
+const urlFmt = "http://weather.livedoor.com/forecast/webservice/json/v1?city=%s" // e.g. 100040
+
+type res struct {
+	PinpointLocations []struct {
+		Link string `json:"link"`
+		Name string `json:"name"`
+	} `json:"pinpointLocations"`
+	Link      string `json:"link"`
+	Forecasts []struct {
+		DateLabel   string `json:"dateLabel"`
+		Telop       string `json:"telop"`
+		Date        string `json:"date"`
+		Temperature struct {
+			Min interface{} `json:"min"`
+			Max struct {
+				Celsius    string `json:"celsius"`
+				Fahrenheit string `json:"fahrenheit"`
+			} `json:"max"`
+		} `json:"temperature"`
+		Image struct {
+			Width  int    `json:"width"`
+			URL    string `json:"url"`
+			Title  string `json:"title"`
+			Height int    `json:"height"`
+		} `json:"image"`
+	} `json:"forecasts"`
+	Location struct {
+		City       string `json:"city"`
+		Area       string `json:"area"`
+		Prefecture string `json:"prefecture"`
+	} `json:"location"`
+	PublicTime string `json:"publicTime"`
+	Copyright  struct {
+		Provider []struct {
+			Link string `json:"link"`
+			Name string `json:"name"`
+		} `json:"provider"`
+		Link  string `json:"link"`
+		Title string `json:"title"`
+		Image struct {
+			Width  int    `json:"width"`
+			Link   string `json:"link"`
+			URL    string `json:"url"`
+			Title  string `json:"title"`
+			Height int    `json:"height"`
+		} `json:"image"`
+	} `json:"copyright"`
+	Title       string `json:"title"`
+	Description struct {
+		Text       string `json:"text"`
+		PublicTime string `json:"publicTime"`
+	} `json:"description"`
+}
+
+// Weather gets weather info
+func Weather(c echo.Context) error {
+	param := c.Param("id")
+
+	url := fmt.Sprintf(urlFmt, param)
+	req, _ := http.NewRequest("GET", url, nil)
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	r := new(res)
+	json.NewDecoder(resp.Body).Decode(r)
+	if r.Title == "" {
+		return echo.ErrNotFound
+	}
+
+	return c.JSON(http.StatusOK, r)
+}
